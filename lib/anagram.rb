b8818fed7a5e4b31b9359d93078dae47d192aecd
Lauren Perchitti <laurenperchitti@gmail.com>
2018-03-28 17:57:22 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,3 +1,17 @@
 # Your code goes here!
 class Anagram
+
+ARRAY = []
+
+  def initialize(word)
+    @word = word
+    ARRAY << word
+  end
+
+  def match(word)
+    word.each do |words|
+      match.%w(words)
+
+  end
+
 end
